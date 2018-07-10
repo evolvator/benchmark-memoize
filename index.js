fd87d5f55af9a9b595d1cb0572bf6de6b868e8b1
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-07-10 09:43:51 +0300

    init

--- index.js
+++ index.js
@@ -1,62 +1,308 @@
 var Benchmark = require('benchmark');
 var tb = require('travis-benchmark');
+var async = require('async');
+var _ = require('lodash');
 
-var utils = require('./utils.js');
+var memoizeOne = require('memoize-one');
+var lruMemoize = require('lru-memoize').default;
+var fastMemoize = require('fast-memoize');
+var memoizee = require('memoizee');
+var memoizeWeak = require('memoize-weak');
+var memoizerific = require('memoizerific');
+var microMemoize = require('micro-memoize').default;
+var moize = require('moize').default;
 
-var n1 = utils.n1;
-var n2 = utils.n2;
-var n3 = utils.n3;
-var n4 = utils.n4;
-var s1 = utils.s1;
-var s2 = utils.s2;
-var s3 = utils.s3;
-var s4 = utils.s4;
-var o1 = utils.o1;
-var o2 = utils.o2;
-var a1 = utils.a1;
-var a2 = utils.a2;
-var f = function() {};
+function objectFunction(config) {
+  return config.a * config.b;
+};
 
-var suite = new Benchmark.Suite(`built-in`);
+async.series([
+  (next) => {
+    var suite = new Benchmark.Suite(`memoize new`);
+    
+    (() => {
+      suite.add({
+        name: 'lodash@4.17.10 memoize',
+        fn: function() {
+          _.memoize(objectFunction);
+        }
+      });
+    })();
+    
+    (() => {
+      suite.add({
+        name: 'memoize-one@4.0.0',
+        fn: function() {
+          memoizeOne(objectFunction);
+        }
+      });
+    })();
+    
+    (() => {
+      suite.add({
+        name: 'lru-memoize@1.0.2',
+        fn: function() {
+          lruMemoize()(objectFunction);
+        }
+      });
+    })();
+    
+    (() => {
+      suite.add({
+        name: 'fast-memoize@2.5.1',
+        fn: function() {
+          fastMemoize(objectFunction);
+        }
+      });
+    })();
+    
+    (() => {
+      suite.add({
+        name: 'memoizee@0.4.12',
+        fn: function() {
+          memoizee(objectFunction);
+        }
+      });
+    })();
+    
+    (() => {
+      suite.add({
+        name: 'memoize-weak@1.0.2',
+        fn: function() {
+          memoizeWeak(objectFunction);
+        }
+      });
+    })();
+    
+    (() => {
+      suite.add({
+        name: 'memoizerific@1.11.3',
+        fn: function() {
+          memoizerific(1)(objectFunction);
+        }
+      });
+    })();
+    
+    (() => {
+      suite.add({
+        name: 'micro-memoize@2.0.3',
+        fn: function() {
+          microMemoize(objectFunction);
+        }
+      });
+    })();
+    
+    (() => {
+      suite.add({
+        name: 'moize@5.3.1',
+        fn: function() {
+          moize(objectFunction);
+        }
+      });
+    })();
 
-require('./new.js')(suite);
-require('./operators.js')(suite);
+    tb.wrapSuite(suite, () => next());
+    suite.run({ async: true });
+  },
+  (next) => {
+    var suite = new Benchmark.Suite(`memoize set by single object argument`);
+    
+    (() => {
+      var memoized = _.memoize(objectFunction);
+      suite.add({
+        name: 'lodash@4.17.10 memoize',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+    
+    (() => {
+      var memoized = memoizeOne(objectFunction);
+      suite.add({
+        name: 'memoize-one@4.0.0',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+    
+    (() => {
+      var memoized = lruMemoize()(objectFunction);
+      suite.add({
+        name: 'lru-memoize@1.0.2',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+    
+    (() => {
+      var memoized = fastMemoize(objectFunction);
+      suite.add({
+        name: 'fast-memoize@2.5.1',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+    
+    (() => {
+      var memoized = memoizee(objectFunction);
+      suite.add({
+        name: 'memoizee@0.4.12',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+    
+    (() => {
+      var memoized = memoizeWeak(objectFunction);
+      suite.add({
+        name: 'memoize-weak@1.0.2',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+    
+    (() => {
+      var memoized = memoizerific(1)(objectFunction);
+      suite.add({
+        name: 'memoizerific@1.11.3',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+    
+    (() => {
+      var memoized = microMemoize(objectFunction);
+      suite.add({
+        name: 'micro-memoize@2.0.3',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+    
+    (() => {
+      var memoized = moize(objectFunction);
+      suite.add({
+        name: 'moize@5.3.1',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
 
-suite.add('if (true) { null; } else { null; }', function() {
-  if (true) {
-    null;
-  } else {
-    null;
+    tb.wrapSuite(suite, () => next());
+    suite.run({ async: true });
+  },
+  (next) => {
+    var suite = new Benchmark.Suite(`memoize get by single object argument`);
+    
+    (() => {
+      var memoized = _.memoize(objectFunction);
+      memoized({ a: 3, b: 7 });
+      suite.add({
+        name: 'lodash@4.17.10 memoize',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+    
+    (() => {
+      var memoized = memoizeOne(objectFunction);
+      memoized({ a: 3, b: 7 });
+      suite.add({
+        name: 'memoize-one@4.0.0',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+    
+    (() => {
+      var memoized = lruMemoize()(objectFunction);
+      memoized({ a: 3, b: 7 });
+      suite.add({
+        name: 'lru-memoize@1.0.2',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+    
+    (() => {
+      var memoized = fastMemoize(objectFunction);
+      memoized({ a: 3, b: 7 });
+      suite.add({
+        name: 'fast-memoize@2.5.1',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+    
+    (() => {
+      var memoized = memoizee(objectFunction);
+      memoized({ a: 3, b: 7 });
+      suite.add({
+        name: 'memoizee@0.4.12',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+    
+    (() => {
+      var memoized = memoizeWeak(objectFunction);
+      memoized({ a: 3, b: 7 });
+      suite.add({
+        name: 'memoize-weak@1.0.2',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+    
+    (() => {
+      var memoized = memoizerific(1)(objectFunction);
+      memoized({ a: 3, b: 7 });
+      suite.add({
+        name: 'memoizerific@1.11.3',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+    
+    (() => {
+      var memoized = microMemoize(objectFunction);
+      memoized({ a: 3, b: 7 });
+      suite.add({
+        name: 'micro-memoize@2.0.3',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+    
+    (() => {
+      var memoized = moize(objectFunction);
+      memoized({ a: 3, b: 7 });
+      suite.add({
+        name: 'moize@5.3.1',
+        fn: function() {
+          memoized({ a: 3, b: 7 });
+        }
+      });
+    })();
+
+    tb.wrapSuite(suite, () => next());
+    suite.run({ async: true });
   }
-});
-
-suite.add('if (false) { null; } else { null; }', function() {
-  if (false) {
-    null;
-  } else {
-    null;
-  }
-});
-
-suite.add('true ? null : null', function() {
-  true ? null : null
-});
-
-suite.add('false ? null : null', function() {
-  false ? null : null
-});
-
-suite.add('eval("")', function() {
-  eval('');
-});
-
-suite.add('typeof [undefined]', function() { typeof undefined; });
-suite.add('typeof [null]', function() { typeof null; });
-suite.add('typeof [number]', function() { typeof n3; });
-suite.add('typeof [string]', function() { typeof s3; });
-suite.add('typeof [object]', function() { typeof o2; });
-suite.add('typeof [array]', function() { typeof a2; });
-suite.add('typeof [function]', function() { typeof f; });
-
-tb.wrapSuite(suite);
-suite.run({ async: true });
+]);
